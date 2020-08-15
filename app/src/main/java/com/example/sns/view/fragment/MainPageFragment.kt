834b0cb5a91ddd4f50a244dbd8410b3e49f8437d
LMY <lmun768@naver.com>
2020-08-15 23:01:23 +0900

    코드 정리

--- app/src/main/java/com/example/sns/view/fragment/MainPageFragment.kt
+++ app/src/main/java/com/example/sns/view/fragment/MainPageFragment.kt
@@ -1,21 +1,9 @@
 package com.example.sns.view.fragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.sns.R
 import com.example.sns.base.BaseFragment
 import com.example.sns.databinding.FragmentMainpageBinding
-import com.example.sns.databinding.FragmentMypageBinding
 import com.example.sns.viewModel.MainPageViewModel
-import com.example.sns.viewModel.MyPageViewModel
-import com.example.sns.widget.MyApplication
-import kotlinx.android.synthetic.main.fragment_mainpage.*
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class MainPageFragment : BaseFragment<FragmentMainpageBinding, MainPageViewModel>() {
--- app/src/main/java/com/example/sns/view/fragment/MapFragment.kt
+++ app/src/main/java/com/example/sns/view/fragment/MapFragment.kt
@@ -1,18 +1,8 @@
 package com.example.sns.view.fragment
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.sns.R
 import com.example.sns.base.BaseFragment
-import com.example.sns.databinding.FragmentMainpageBinding
 import com.example.sns.databinding.FragmentMapBinding
-import com.example.sns.viewModel.MainPageViewModel
 import com.example.sns.viewModel.MapViewModel
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
