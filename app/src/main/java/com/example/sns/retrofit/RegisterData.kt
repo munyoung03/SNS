654157d97b7e831d39925d4092c63757759de958
LMY <lmun768@naver.com>
2020-09-03 09:53:38 +0900

    [UPDATE] RegisterData

--- app/src/main/java/com/example/sns/retrofit/RegisterData.kt
+++ app/src/main/java/com/example/sns/retrofit/RegisterData.kt
@@ -1,14 +1,5 @@
 package com.example.sns.retrofit
 
 data class Register(
-    val token : String,
-    val user: User
-) {
-    data class User(
-        val pk : String,
-        val username : String,
-        val email: String,
-        val first_name: String,
-        val last_name: String
-    )
-}
+    val key : String
+)
